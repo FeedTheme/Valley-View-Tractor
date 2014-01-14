5a2827c1b46722afeecda3fbad890f4afaac091d
Justin Sainton <justinsainton@gmail.com>
2014-01-14 15:01:30 -0800

    Whoops, forgot to change slugs.

--- functions.php
+++ functions.php
@@ -80,8 +80,8 @@ function vvt_scripts() {
 
 	wp_enqueue_style( 'vvt-style', get_stylesheet_uri() );
     wp_enqueue_style( 'google-font-archivo', $scheme . 'fonts.googleapis.com/css?family=Archivo+Narrow:700');
-    wp_enqueue_style( 'google-font-archivo', $scheme . 'fonts.googleapis.com/css?family=Droid+Sans');
-    wp_enqueue_style( 'google-font-archivo', $scheme . 'fonts.googleapis.com/css?family=Courgette');
+    wp_enqueue_style( 'google-font-droid-sans', $scheme . 'fonts.googleapis.com/css?family=Droid+Sans');
+    wp_enqueue_style( 'google-font-courgette', $scheme . 'fonts.googleapis.com/css?family=Courgette');
 
 	wp_enqueue_script( 'vvt-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
